b8d485cbb1d6c090b80f235ad1799e6341b547e0
jrg94 <jrg170@case.edu>
2016-02-17 13:00:07 -0500

    Added getters and setters

--- EECS398_UI/app/src/main/java/com/example/christen/eecs398_ui/SmartLock.java
+++ EECS398_UI/app/src/main/java/com/example/christen/eecs398_ui/SmartLock.java
@@ -7,9 +7,14 @@ package com.example.christen.eecs398_ui;
  */
 public class SmartLock {
 
+    // FIELDS //
+
     private int id;
     private String address;
     private GPSLocation location;
+    private boolean isLocked;
+
+    // CONSTRUCTORS //
 
     public SmartLock(int id, GPSLocation location) {
         this.id = id;
@@ -21,5 +26,64 @@ public class SmartLock {
         this.id = id;
         this.location = new GPSLocation(latitude, longitude);
         // Determine address from GPSLocation
+    }
+
+    // GETTER/SETTERS //
+
+    /**
+     * The ID getter method
+     * There exists no setter method because we do not
+     * want to be able to change the ID for this lock
+     * @return the identification number for this lock
+     */
+    public int getID() {
+        return this.id;
+    }
+
+    /**
+     * The address getter method
+     * @return the address of this lock
+     */
+    public String getAddress() {
+        return this.address;
+    }
+
+    /**
+     * The address setter method
+     * @param address a string that marks the general location of the lock
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * The location getter method
+     * @return the GPSLocation for the lock
+     */
+    public GPSLocation getLocation() {
+        return this.location;
+    }
+
+    /**
+     * The location setter method
+     * @param location the GPSLocation of the lock
+     */
+    public void setLocation(GPSLocation location) {
+        this.location = location;
+    }
+
+    // FUNCTIONALITY //
+
+    /**
+     * Toggles the state of the lock
+     * @return the state of the lock after the toggle
+     */
+    public boolean toggleLock() {
+        /**
+         * TODO: Add functionality to the UI so users can visually see the state of the lock
+         * TODO: This code may not get added here, but be aware that it will need to be added
+         */
+        isLocked = !isLocked;
+        return isLocked;
     }
 }
