8ab6cf2f441311fd38bbb754435871093f704e49
jrg94 <jrg170@case.edu>
2016-02-18 16:34:15 -0500

    Starting fresh with the main activity

--- EECS398_UI/app/src/main/java/com/example/christen/eecs398_ui/MainActivity.java
+++ EECS398_UI/app/src/main/java/com/example/christen/eecs398_ui/MainActivity.java
@@ -3,38 +3,44 @@ package com.example.christen.eecs398_ui;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.util.Set;
 
-
 public class MainActivity extends Activity {
 
-    private final static int REQUEST_ENABLE_BT = 1;
-    private final BroadcastReceiver receiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            // When discovery finds a device
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                // Get the bluetooth object from the intent
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                // TODO: Do something with the list of devices (See Android.Bluetooth documentation)
-            }
-        }
-    };
+    // Debug tools
+    private static final boolean DEBUG = true;
+    private static final String TAG = "MainActivity";
+
+    private BluetoothAdapter mBluetoothAdapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (DEBUG) {
+            Log.e(TAG, ">>> ON CREATE <<<");
+        }
+
+        // Sets up the window layout
         setContentView(R.layout.activity_main);
-        setupBluetooth();
+
+        // Retrieves the local bluetooth adapter
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        // If bluetooth is not supported
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
     }
 
     @Override
@@ -57,59 +63,5 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * Sets up the bluetooth on this device
-     *
-     * @return true if bluetooth setup was successful
-     */
-    public boolean setupBluetooth() {
-        System.out.println("Setting up Bluetooth");
-        BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
-        System.out.println("Acquired the default adapter");
-
-        // Checks to see if bluetooth is supported by this device
-        // Emulator cannot test this
-        if (adapter == null) {
-            // TODO: Throw an exception
-            System.out.println("The adapter is null");
-            return false;
-        }
-
-        // Checks to see if bluetooth is enabled
-        if (!adapter.isEnabled()) {
-            System.out.println("The adapter is not enabled");
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-        }
-
-        // Query list of paired devices
-        Set<BluetoothDevice> pairedDevices = adapter.getBondedDevices();
-        // If there are paired devices
-        if (pairedDevices.size() > 0) {
-            // Loop through list of devices
-            for (BluetoothDevice device : pairedDevices) {
-                // TODO: Do something with the list of devices (See Android.Bluetooth documentation
-            }
-        }
-
-        // Register the BroadcastReceiver
-        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        registerReceiver(receiver, filter);
-        // TODO: Unregister during OnDestroy
-
-        return true;
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_ENABLE_BT && resultCode == Activity.RESULT_OK) {
-            //BluetoothAdapter BT = BluetoothAdapter.getDefaultAdapter();
-            //String address = BT.getAddress();
-            //String name = BT.getName();
-            //String toastText = name + " : " + address;
-            //Toast.makeText(this, toastText, Toast.LENGTH_LONG).show();
-        }
     }
 }
