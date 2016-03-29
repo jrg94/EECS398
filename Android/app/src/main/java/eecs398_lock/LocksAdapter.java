d1adeed98f10e387a0166bf03fdbe087f5a7e31a
jrg94 <jrg170@case.edu>
2016-03-29 16:25:56 -0400

    Refined lock UI to only display name and slider

--- Android/app/src/main/java/eecs398_lock/LocksAdapter.java
+++ Android/app/src/main/java/eecs398_lock/LocksAdapter.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import app.lock.bluetooth.smart_lock_app.LockListScreen;
@@ -42,23 +44,29 @@ public class LocksAdapter extends ArrayAdapter<SmartLock> {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.lock_ui, parent, false);
         }
 
+        // Initialize UI elements
         TextView lockLabel = (TextView) convertView.findViewById(R.id.lockLabel);
-        TextView lockID = (TextView) convertView.findViewById(R.id.lockID);
-        TextView lockLat = (TextView) convertView.findViewById(R.id.lockLat);
-        TextView lockLong = (TextView) convertView.findViewById(R.id.lockLong);
+        Switch lockStatus = (Switch) convertView.findViewById(R.id.lockState);
         final Button popupMenuButton = (Button) convertView.findViewById(R.id.popup_lock_menu_button);
 
-        lockLabel.setText(String.format("%s: %s", "Label", lock.getLabel()));
-        lockID.setText(String.format("%s: %s", "ID", lock.getID().toString()));
-        lockLat.setText(String.format("%s: %f", "Latitude", lock.getLocation().getLatitude()));
-        lockLong.setText(String.format("%s: %f", "Longitude", lock.getLocation().getLongitude()));
+        // Set the UI elements up
+        lockLabel.setText(lock.getLabel());
+
+        lockStatus.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    lock.toggleLock((LockListScreen)mContext);
+                }
+            }
+        });
+
         popupMenuButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (mContext instanceof LockListScreen) {
-                    LockListScreen lls = (LockListScreen)mContext;
+                    LockListScreen lls = (LockListScreen) mContext;
                     lls.showPopUp(lls.findViewById(R.id.gridView), lock);
-                    lock.toggleLock(lls);
                 }
             }
         });
