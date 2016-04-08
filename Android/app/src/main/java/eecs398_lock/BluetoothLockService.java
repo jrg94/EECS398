1e311de8fb00b972e2c778e9af499f3f25e21923
jrg94 <jrg170@case.edu>
2016-04-08 11:15:37 -0400

    Toast messages now read full text before outputting

--- Android/app/src/main/java/eecs398_lock/BluetoothLockService.java
+++ Android/app/src/main/java/eecs398_lock/BluetoothLockService.java
@@ -449,15 +449,23 @@ public class BluetoothLockService {
         public void run() {
             Log.i(TAG, "BEGIN mConnectedThread");
             byte[] buffer = new byte[1024];
-            int bytes;
+            int bytes = 0;
 
             // Keep listening to the InputStream while connected
             while (true) {
                 try {
                     // Read from the InputStream
-                    bytes = mmInStream.read(buffer);
-                    // Send the obtained bytes to the UI Activity
-                    mHandler.obtainMessage(LockListScreen.LOCK_READ, bytes, -1, buffer).sendToTarget();
+                    buffer[bytes] = (byte) mmInStream.read();
+
+                    // Checks for new line before outputting any data
+                    if ((buffer[bytes] == '\n')) {
+                        // Send the obtained bytes to the UI Activity
+                        mHandler.obtainMessage(LockListScreen.LOCK_READ, bytes, -1, buffer).sendToTarget();
+                        bytes = 0;
+                    }
+                    else {
+                        bytes++;
+                    }
                 } catch (IOException e) {
                     Log.e(TAG, "disconnected", e);
                     connectionLost();
