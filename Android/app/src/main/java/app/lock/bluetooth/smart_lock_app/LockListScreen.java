8ed9ba82e3cb5a4a473fde798006e9711eb2ef4a
jrg94 <jrg170@case.edu>
2016-04-17 17:21:19 -0400

    Cleaned code
    Also added permission for location services

--- Android/app/src/main/java/app/lock/bluetooth/smart_lock_app/LockListScreen.java
+++ Android/app/src/main/java/app/lock/bluetooth/smart_lock_app/LockListScreen.java
@@ -22,6 +22,7 @@ import android.bluetooth.BluetoothDevice;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Point;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -71,18 +72,12 @@ public class LockListScreen extends Activity {
     private static final int REQUEST_CONNECT_DEVICE = 1;
     private static final int REQUEST_ENABLE_BT = 2;
 
-    // Local Bluetooth adapter
-    private BluetoothAdapter mBluetoothAdapter = null;
-
-    // Member object for the chat services
-    public BluetoothLockService mLockService = null;
-
-    // The manager of all the locks
-    private SmartLockManager lockManager = null;
-
-    private LocksAdapter mLockArrayAdapter;
-
-    private String currentAddress = "??:??:??:??:??:??";
+    public BluetoothLockService mLockService = null;        // Member object for the chat services
+    private BluetoothAdapter mBluetoothAdapter = null;      // Local Bluetooth adapter
+    private SmartLockManager lockManager = null;            // The manager of all the locks
+    private LocksAdapter mLockArrayAdapter;                 // The adapter for the grid of locks
+    private String currentAddress = "??:??:??:??:??:??";    // An empty address
+    private LocationManager locationManager = null;         // Handles location services for the app
 
     /**
      * The onCreate method which can be overridden in all Activity classes
