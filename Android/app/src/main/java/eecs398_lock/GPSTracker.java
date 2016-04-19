aba1f2e9aa3bac19e935385310263b458a2de0ed
jrg94 <jrg170@case.edu>
2016-04-19 11:06:17 -0400

    Cleaned up GPSTracker code

--- Android/app/src/main/java/eecs398_lock/GPSTracker.java
+++ Android/app/src/main/java/eecs398_lock/GPSTracker.java
@@ -12,29 +12,50 @@ import java.util.Collection;
 
 /**
  * Created by JRG94 on 4/17/2016.
+ *
+ * A class which implements the LocationListener interface
  */
 public class GPSTracker implements LocationListener {
 
+    /* Brief list of helpful fields */
     private Context mContext;
-    private Collection<SmartLock> locks;
+    private Collection<SmartLock> locks;    // The collection of locks
     private double lastLatitude;
     private double lastLongitude;
 
+    /* Constant for specifying the trigger distance from the lock */
     private static final double DIST_FROM_LOCK_TO_KEY = 50;
 
+    /**
+     * The default constructor for the LocationListener
+     * @param mContext the app's context
+     * @param locks the list of locks
+     */
     public GPSTracker(Context mContext, Collection<SmartLock> locks) {
         this.mContext = mContext;
         this.locks = locks;
     }
 
+    /**
+     * Retrieves the last recorded latitude
+     * @return the lastLatitude field
+     */
     public double getLastLatitude() {
         return lastLatitude;
     }
 
+    /**
+     * Retrieves the last recorded longitude
+     * @return the lastLongitude field
+     */
     public double getLastLongitude() {
         return lastLongitude;
     }
 
+    /**
+     * Updates fields on location change
+     * @param location the object containing data about the current location
+     */
     @Override
     public void onLocationChanged(Location location) {
 
@@ -56,20 +77,35 @@ public class GPSTracker implements LocationListener {
         }
     }
 
+    /**
+     * Currently not implemented
+     * @param provider the gps/network provider
+     * @param status the status code
+     * @param extras a bundle of extra data
+     */
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
 
     }
 
+    /**
+     * Prints a message when GPS gets enabled
+     * @param provider the network/gps provider
+     */
     @Override
     public void onProviderEnabled(String provider) {
-        Toast.makeText(mContext, "Gps is turned on!! ", Toast.LENGTH_SHORT).show();
+        Toast.makeText(mContext, "GPS has been activated", Toast.LENGTH_SHORT).show();
     }
 
+    /**
+     * Asks the user to enable GPS and prints a message to let the user
+     * know that GPS has been deactivated
+     * @param provider the network/gps provider
+     */
     @Override
     public void onProviderDisabled(String provider) {
         Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
         mContext.startActivity(intent);
-        Toast.makeText(mContext, "Gps is turned off!! ", Toast.LENGTH_SHORT).show();
+        Toast.makeText(mContext, "GPS has been deactivated", Toast.LENGTH_SHORT).show();
     }
 }
