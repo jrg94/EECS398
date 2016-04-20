3f021900f8ff7625197f346a249a82ada8c2638f
jrg94 <jrg170@case.edu>
2016-04-20 11:58:00 -0400

    Removed a contains line
    Caused a null pointer - looking to fix this issue later

--- Android/app/src/main/java/app/lock/bluetooth/smart_lock_app/DeviceListActivity.java
+++ Android/app/src/main/java/app/lock/bluetooth/smart_lock_app/DeviceListActivity.java
@@ -187,7 +187,7 @@ public class DeviceListActivity extends Activity {
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
 
                 // If it's already paired, skip it, because it's been listed already
-                if (device.getBondState() != BluetoothDevice.BOND_BONDED && device.getName().contains("Adafruit")) {
+                if (device.getBondState() != BluetoothDevice.BOND_BONDED) {
                     mNewDevicesArrayAdapter.add(device.getName() + "\n" + device.getAddress());
                 }
                 // When discovery is finished, change the Activity title
