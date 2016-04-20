c22a75f52ac0ec9c5cbc1580b83ff42a5c6b33ae
jrg94 <jrg170@case.edu>
2016-04-20 13:13:53 -0400

    Ran into a problem with testing getView

--- Android/app/src/androidTest/java/app/lock/bluetooth/smart_lock_app/LocksAdapterTest.java
+++ Android/app/src/androidTest/java/app/lock/bluetooth/smart_lock_app/LocksAdapterTest.java
@@ -1,6 +1,10 @@
 package app.lock.bluetooth.smart_lock_app;
 
 import android.test.AndroidTestCase;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import java.util.HashMap;
 
@@ -13,6 +17,7 @@ import eecs398_lock.SmartLock;
 public class LocksAdapterTest extends AndroidTestCase {
 
     private LocksAdapter mAdapter;
+    private HashMap<String, SmartLock> data;
 
     private static final String testMACAddres = "AA:BB:CC:DD:EE:FF";
 
@@ -20,16 +25,41 @@ public class LocksAdapterTest extends AndroidTestCase {
         super();
     }
 
+    /**
+     * Initializes a test adapter
+     * @throws Exception
+     */
     protected void setUp() throws Exception {
         super.setUp();
-        HashMap<String, SmartLock> data = new HashMap<String, SmartLock>();
+        data = new HashMap<String, SmartLock>();
 
         data.put(testMACAddres, new SmartLock(testMACAddres));
 
         mAdapter = new LocksAdapter(getContext(), data);
     }
 
+    /**
+     * Tests that the adapter returns the correct element count
+     */
     public void testGetCount() {
-        assertEquals("Lock count incorrect", 1, mAdapter.getCount());
+        assertEquals("Lock count incorrect", data.size(), mAdapter.getCount());
     }
+
+    /**
+    public void testGetView() {
+        View view = mAdapter.getView(0, null, null);
+
+        // Initialize the UI elements
+        TextView lockLabel = (TextView) view.findViewById(R.id.lockLabel);
+        TextView connectedStatus = (TextView) view.findViewById(R.id.connectedStatus);
+        Switch lockStatus = (Switch) view.findViewById(R.id.lockState);
+        Button popupMenuButton = (Button) view.findViewById(R.id.popup_lock_menu_button);
+
+        // Test that all views exist
+        assertNotNull("View is null", view);
+        assertNotNull("LockLabel TextView is null", lockLabel);
+        assertNotNull("ConnectedStatus TextView is null", connectedStatus);
+        assertNotNull("LockStatus Switch is null", lockStatus);
+        assertNotNull("PopupMenuButton Button is null", popupMenuButton);
+    }*/
 }
